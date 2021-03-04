2e14b3968c7b33b85b30870164238f179ccc5b18
André Abou Chami Campana <andreabou@me.com>
2021-03-04 11:29:07 +0100

    Initial commit

---
+++ Example/Kalmander_iOS/Kalmander_iOS.h
@@ -0,0 +1,18 @@
+//
+//  Kalmander_iOS.h
+//  Kalmander_iOS
+//
+//  Created by André Campana on 04.03.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Kalmander_iOS.
+FOUNDATION_EXPORT double Kalmander_iOSVersionNumber;
+
+//! Project version string for Kalmander_iOS.
+FOUNDATION_EXPORT const unsigned char Kalmander_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Kalmander_iOS/PublicHeader.h>
+
+
